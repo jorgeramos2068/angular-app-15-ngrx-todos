4edc4929c0fe308ae3b2ae32c0bcc29a7e6aaf04
Jorge Ramos <jramos@intersysconsulting.com>
2022-01-31 18:11:47 -0600

    Perform initial configurations

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoPageComponent } from './pages/todo-page/todo-page.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodoFooterComponent } from './components/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AddTodoComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent,
+  ],
+  imports: [CommonModule],
+})
+export class TodosModule {}
