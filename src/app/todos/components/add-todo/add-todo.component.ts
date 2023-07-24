3fab788eadca4563efd7aeb21687da15092759b0
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:51:51 -0600

    Perform initial configurations

---
+++ src/app/todos/components/add-todo/add-todo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styles: [
+  ]
+})
+export class AddTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
