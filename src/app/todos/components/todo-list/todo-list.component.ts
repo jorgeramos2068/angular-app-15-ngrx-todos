3cc071c53f16ac627ad69cfc30048cc4fe3a923f
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:51:53 -0600

    Add functionality to toggle a todo completed state

--- src/app/todos/components/todo-list/todo-list.component.ts
+++ src/app/todos/components/todo-list/todo-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { Todo } from '../../models/todo.model';
+import { AppState } from 'src/app/app.state';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styles: [
-  ]
 })
 export class TodoListComponent implements OnInit {
+  public todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.select('todos').subscribe((todos) => (this.todos = todos));
   }
-
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -3,11 +3,26 @@ import { Action, createReducer, on } from '@ngrx/store';
 import { Todo } from './models/todo.model';
 import * as actions from './todo.actions';
 
-export const initialState: Todo[] = [];
+export const initialState: Todo[] = [
+  new Todo('Save the World'),
+  new Todo('Help the Avengers'),
+  new Todo('Wash my dishes'),
+];
 
 const _todoReducer = createReducer(
   initialState,
-  on(actions.createTodo, (state, { text }) => [...state, new Todo(text)])
+  on(actions.createTodo, (state, { text }) => [...state, new Todo(text)]),
+  on(actions.toggleTodo, (state, { id }) => {
+    return state.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+  })
 );
 
 export function todoReducer(state: any, action: Action) {
